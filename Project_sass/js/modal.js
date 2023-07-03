fdbd43a08ee48038533e4b5ccd4e6624c4038a30
Morfay <132558248+Morfay@users.noreply.github.com>
2023-07-03 23:52:49 +0300

    Mobile version is added

---
+++ Project_sass/js/modal.js
@@ -0,0 +1,23 @@
+        const refs = {
+            openModalBtns: document.querySelectorAll("[data-open-modal]"),
+            closeModalBtn: document.querySelector("[data-close-modal]"),
+            backdrop: document.querySelector("[data-backdrop]"),
+        };
+
+        console.log(refs.openModalBtns);
+
+        refs.openModalBtns.forEach(btn => {
+            btn.addEventListener("click", toggleModal);
+        });
+
+        refs.closeModalBtn.addEventListener("click", toggleModal);
+
+        refs.backdrop.addEventListener("click", logBackdropClick);
+
+        function toggleModal() {
+            refs.backdrop.classList.toggle("is-hidden");
+        }
+
+        function logBackdropClick() {
+            console.log("Open backdrop");
+        }
